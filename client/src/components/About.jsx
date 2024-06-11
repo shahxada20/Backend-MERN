0e6f508df05132c0a3ae16e61ab2201ac51eb5f1
Shahzad <shahzadarain144@gmail.com>
2024-06-11 13:25:08 +0500

    clean Interface

--- client/src/components/About.jsx
+++
@@ -1,5 +0,0 @@
-const About  = () => {
-return( <p>About Page</p>
-)}
-
-export default About;
--- client/src/components/Service.jsx
+++
@@ -1,5 +0,0 @@
-const Service  = () => {
-    return( <p>Service Page</p>
-    )}
-    
-    export default Service;
